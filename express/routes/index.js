1fd4ebd0f9241ae73defd0d8e5b7f60fc8692ae5
Victor Wade <p9859817888@gmail.com>
2024-10-30 13:35:46 -1000

    init routes middleware, boilerplate

--- express/routes/index.js
+++ express/routes/index.js
@@ -11,8 +11,9 @@ router.use(require("morgan")("dev"));
 router.use(express.json());
 
 router.use(authRoutes);
-router.use("/playlists", authenticate, require("./playlists")); // this entire path requires authentication
-router.use("/tracks", require("./tracks"));
+router.use("/projects", authenticate, require("./projects")); // this entire path requires authentication
+router.use("/info", authenticate, require("./student"));
+router.use("/profile", require("./student/profile"));
 
 module.exports = {
 	routes: router,
