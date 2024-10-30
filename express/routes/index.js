937ebd8c30eeccd45521b75e504204b0f90a8515
Victor Wade <p9859817888@gmail.com>
2024-10-30 13:28:46 -1000

    express setup, folder structure

---
+++ express/routes/index.js
@@ -0,0 +1,20 @@
+// express/routes/index.js
+
+const express = require("express");
+const router = express.Router();
+
+const { authRoutes, authenticate } = require("../api/auth");
+const { errorHandler } = require("../middleware");
+const auth = require("../api/auth");
+
+router.use(require("morgan")("dev"));
+router.use(express.json());
+
+router.use(authRoutes);
+router.use("/playlists", authenticate, require("./playlists")); // this entire path requires authentication
+router.use("/tracks", require("./tracks"));
+
+module.exports = {
+	routes: router,
+	errorHandler,
+};
