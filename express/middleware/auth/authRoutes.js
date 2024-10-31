5e83ade7f08e21118acee88ec3f8731fc9253bd5
Victor Wade <p9859817888@gmail.com>
2024-10-30 15:00:14 -1000

    auth routes, init & left blank

---
+++ express/middleware/auth/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { createToken, verifyToken } = require("./token");
+
+// TODO -- add routes for token verification and user registration/login
+
+module.exports = router;
