a6dafa4233e7350c010faff0abb529bcf275597e
Victor Wade <p9859817888@gmail.com>
2024-10-30 15:01:03 -1000

    authentication middleware, left blank

--- express/middleware/auth/authenticate.js
+++ express/middleware/auth/authenticate.js
@@ -1,5 +1,2 @@
 // authentication function
-module.exports = (req, res, next) => {
-	if (req.user) next();
-	else next({ status: 401, message: "Unauthorized." });
-}; // intentionally left blank
+module.exports = (req, res, next) => {}; // intentionally left blank
