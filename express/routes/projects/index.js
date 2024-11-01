1a030d2764ddbc9f69c617c258706745399210c6
Victor Wade <p9859817888@gmail.com>
2024-11-01 06:49:24 -1000

    project `POST` create new assignment

--- express/routes/projects/index.js
+++ express/routes/projects/index.js
@@ -29,4 +29,32 @@ router.get("/:id", async (req, res, next) => {
 	}
 });
 
+router.post("/:id", async (req, res, next) => {
+	try {
+		const id = Number(req.params.id);
+
+		// check if already enrolled
+		if (
+			await prisma.assignment.findUnique({
+				where: {
+					studentId: req.user.id,
+					currentProjectId: id,
+				},
+			})
+		)
+			return next({ error: 401, message: "Assignment already exists" });
+
+		// create new assignment
+		const assignment = await prisma.assignment.create({
+			data: {
+				grade: 0,
+				studentId: { connect: { id: req.user.id } },
+				currentProjectId: { connect: { id } },
+			},
+		});
+	} catch (e) {
+		next(e);
+	}
+});
+
 module.exports = router;
