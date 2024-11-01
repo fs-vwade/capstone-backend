19fed4048b8d75e6876ba16cd7d5f74415c78aab
Victor Wade <p9859817888@gmail.com>
2024-10-31 21:54:43 -1000

    get student info
    use with Profile page

--- express/routes/student/index.js
+++ express/routes/student/index.js
@@ -2,5 +2,25 @@
 
 const express = require("express");
 const router = express.Router();
+const prisma = require("../../../prisma");
+
+router.get("/", async (req, res, next) => {
+	try {
+		const student = await prisma.student.findUnique({
+			where: { id: req.user.id },
+			include: { projects },
+			omit: { password: true },
+		});
+		res.status(200).json(student); /**{
+			name: student.name,
+			username: student.username,
+			level: student.level,
+			exp: student.exp,
+			projects: student.projects,
+		} */
+	} catch (e) {
+		next(e);
+	}
+});
 
 module.exports = router;
