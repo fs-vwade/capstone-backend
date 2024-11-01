5a8bb9d4b29cff6e99e9494e2169bce0643b3f6e
Victor Wade <p9859817888@gmail.com>
2024-11-01 05:47:32 -1000

    more path fixes

--- express/routes/projects/index.js
+++ express/routes/projects/index.js
@@ -2,31 +2,31 @@
 
 const express = require("express");
 const router = express.Router();
-const prisma = require("../prisma");
+const prisma = require("../../../prisma");
 const authenticate = require("../../middleware/auth/authenticate");
 
 router.get("/", authenticate, async (req, res, next) => {
-  try {
-    const projects = await prisma.projects.findMany();
-    res.json(projects);
-  } catch (e) {
-    next(e);
-  }
+	try {
+		const projects = await prisma.projects.findMany();
+		res.json(projects);
+	} catch (e) {
+		next(e);
+	}
 });
 
 router.get("/:id", authenticate, async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const projects = await prisma.projects.findUniqueOrThrow({
-      where: { id: +id },
-    });
-    if (projects.userId !== req.user.id) {
-      next({ status: 403, message: "You are not logged in" });
-    }
-    res.json(projects);
-  } catch (e) {
-    next(e);
-  }
+	const { id } = req.params;
+	try {
+		const projects = await prisma.projects.findUniqueOrThrow({
+			where: { id: +id },
+		});
+		if (projects.userId !== req.user.id) {
+			next({ status: 403, message: "You are not logged in" });
+		}
+		res.json(projects);
+	} catch (e) {
+		next(e);
+	}
 });
 
 module.exports = router;
