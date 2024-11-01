a8826b40a5051583b6ee935f18814e9a7d2c3065
Victor Wade <p2525657888@gmail.com>
2024-11-01 02:21:38 -1000

    prisma automatically calculates EXP

--- prisma/index.js
+++ prisma/index.js
@@ -1,8 +1,10 @@
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient().$extends({
+const prisma = new PrismaClient();
+
+prisma.$extends({
 	model: {
-		user: {
+		student: {
 			register: async (username, password) =>
 				await prisma.user.create({
 					data: { username, password: await bcrypt.hash(String(password), 10) },
@@ -16,6 +18,35 @@ const prisma = new PrismaClient().$extends({
 			},
 		},
 	},
+});
+
+prisma.$use(async (params, next) => {
+	if (
+		params.model === "Assignment" &&
+		["create", "update", "delete"].includes(params.action)
+	) {
+		const result = await next(params);
+		const studentId = result.studentId;
+		const assignments = await prisma.assignment.findMany({
+			where: { studentId },
+			include: { currentProject: true },
+		});
+
+		await prisma.student.update({
+			where: { id: studentId },
+			data: {
+				exp: assignments.reduce(
+					(exp, assignment) =>
+						exp + (assignment.grade / 100) * assignment.currentProject.exp,
+					0
+				),
+			},
+		});
+
+		return result;
+	}
+
+	return next(params);
 });
 
 module.exports = prisma;
