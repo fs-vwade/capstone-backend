fefc2e4b25790def035b0b46784ac394f8940f10
Victor Wade <p9859817888@gmail.com>
2024-11-01 08:04:01 -1000

    minor syntax issue, but everything is fine

--- express/middleware/auth/authenticate.js
+++ express/middleware/auth/authenticate.js
@@ -1,11 +1,7 @@
 const prisma = require("../../../prisma");
 
 // authentication function
-function authenticate(req, res, next) {
-  if (req.user) {
-    next();
-  } else {
-    next({ status: 401, message: "You must be logged in." });
-  }
-}
-(module.exports = (req, res, next) => {}), authenticate; // intentionally left blank
+module.exports = (req, res, next) => {
+	if (req.user) next();
+	else next({ status: 401, message: "You must be logged in." });
+};
