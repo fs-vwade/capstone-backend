a8403e94b8b9365cd0e6d96dddf93e3f1a2cf3b5
Victor Wade <p9859817888@gmail.com>
2024-11-03 12:31:07 -1000

    submissions test, rest

--- express/routes/submissions/index.js
+++ express/routes/submissions/index.js
@@ -8,10 +8,17 @@ router.put("/", async (req, res, next) => {
 	try {
 		const { studentId, projectId, grade } = req.body;
 		const assignment = await prisma.assignment.update({
-			where: { studentId: req.user.id, currentProjectId: projectId },
+			where: {
+				studentId_currentProjectId: {
+					studentId: studentId,
+					currentProjectId: projectId,
+				},
+			},
+			// this is just to simulate variance in the grading process, remove later
+			data: { grade: grade * 0.5 * Math.pow(2.5, Math.random) },
 		});
-		if (assignment) res.status(200).json(student);
-		else next({ status: 404, message: "Student must be enrolled." });
+		if (assignment) res.status(200).json({ assignment });
+		else next({ status: 403, message: "Cannot grade if not enrolled." });
 	} catch (e) {
 		next(e);
 	}
