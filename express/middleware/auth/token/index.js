d30d2bd3a9b43a1430a6a11115d7815afef97458
Victor Wade <p9859817888@gmail.com>
2024-11-03 06:44:51 -1000

    formatting, troubleshoot tracing

--- express/middleware/auth/token/index.js
+++ express/middleware/auth/token/index.js
@@ -6,9 +6,9 @@ module.exports = {
 	createToken: (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" }),
 	verifyToken: async (req, res, next) => {
 		try {
-			const bearer = req.headers.authorization;
-			const token = bearer?.replace(/(?:Bearer )/, "").trim();
-			console.log(token);
+			const token = req.headers.authorization
+				?.replace(/(?:Bearer )/, "")
+				.trim();
 			if (!token) return next();
 
 			const { id } = jwt.verify(token, JWT_SECRET);
