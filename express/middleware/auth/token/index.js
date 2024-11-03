5d10ac69277fab91c3018db1dfa061f60856db54
Victor Wade <p9859817888@gmail.com>
2024-11-03 06:30:59 -1000

    ensure everything is inside a trycatch from now on

--- express/middleware/auth/token/index.js
+++ express/middleware/auth/token/index.js
@@ -5,10 +5,12 @@ const prisma = require("../../../../prisma");
 module.exports = {
 	createToken: (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" }),
 	verifyToken: async (req, res, next) => {
-		const bearer = String(req.headers.authorization);
-		const token = bearer.replace(/(?: |Bearer *)/, "");
-		if (!token) return next();
 		try {
+			const bearer = req.headers.authorization;
+			const token = bearer?.replace(/(?:Bearer )/, "").trim();
+			console.log(token);
+			if (!token) return next();
+
 			const { id } = jwt.verify(token, JWT_SECRET);
 			const user = await prisma.student.findUniqueOrThrow({ where: { id } });
 			req.user = user;
