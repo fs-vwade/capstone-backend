434ad3ab7eb10aade3c345b4afbbb46162fa5948
Victor Wade <p9859817888@gmail.com>
2024-11-03 09:28:50 -1000

    cleanup, auth testing

--- express/middleware/auth/token/index.js
+++ express/middleware/auth/token/index.js
@@ -9,15 +9,9 @@ module.exports = {
 			const token = req.headers.authorization
 				?.replace(/(?:Bearer )/, "")
 				.trim();
-
-			//console.log("token:", token);
-			//console.log("JWT_SECRET:", JWT_SECRET);
-			//const decodedToken = jwt.decode(token);
-			//console.log("Decoded token:", decodedToken);
 			if (!token) return next();
 
 			const { id } = jwt.verify(token, JWT_SECRET);
-			//console.log(id, JWT_SECRET);
 			const user = await prisma.student.findUniqueOrThrow({ where: { id } });
 			req.user = user;
 			next();
