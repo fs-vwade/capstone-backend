87649d9145b463a02dfdd76775dfd7c382121617
Victor Wade <p9859817888@gmail.com>
2024-11-04 11:05:53 -1000

    grade updated if lesser

--- express/routes/submissions/index.js
+++ express/routes/submissions/index.js
@@ -6,19 +6,30 @@ const prisma = require("../../../prisma");
 
 router.put("/", async (req, res, next) => {
 	try {
-		const { studentId, projectId, grade } = req.body;
-		const assignment = await prisma.assignment.update({
+		const { studentId, projectId } = req.body;
+		// this is just to simulate variance in the grading process, remove later
+		const grade = req.body.grade * 0.5 * Math.pow(2.5, Math.random());
+		const assignment = await prisma.assignment.findUnique({
 			where: {
 				studentId_currentProjectId: {
 					studentId: studentId,
 					currentProjectId: projectId,
 				},
 			},
-			// this is just to simulate variance in the grading process, remove later
-			data: { grade: parseInt(grade * 0.5 * Math.pow(2.5, Math.random())) },
 		});
-		if (assignment) res.status(200).json({ assignment });
-		else next({ status: 403, message: "Cannot grade if not enrolled." });
+		if (assignment) {
+			if (assignment.grade < grade) {
+				const evaluation = await prisma.assignment.update({
+					where: { id: assignment.id },
+					data: { grade },
+				});
+				res.status(200).json({ evaluation });
+			} else {
+				res
+					.status(200)
+					.send(`Existing grade is ${assignment.grade}. No evaluation.`);
+			}
+		} else next({ status: 403, message: "No evaluation." });
 	} catch (e) {
 		next(e);
 	}
